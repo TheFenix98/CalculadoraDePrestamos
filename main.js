e2e48bbedbc05a76d09c8630c7f0466d0f61c4bf
Benjamin Coronel <benjamincoronel98@hotmail.com>
2023-09-06 19:24:23 -0300

    html y js terminados

--- main.js
+++ main.js
@@ -1,35 +1,30 @@
-let prestamo = 0;
-let tasaDeInteres = 0.05;
-let periodo = 0;
-let cuota = 0;
+const tasaDeInteres = 0.05;
+let cuota = 0; // Declaración de cuota fuera del evento submit
 
-while (prestamo <100000) {
-    prestamo= parseInt(prompt("ingrese el monto que desea pedir:"))
-    if (prestamo < 100000){
-        alert("el monto ingresado debe ser mayor a $100.000. intente de nuevo")
+document.getElementById("miFormulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const prestamo = parseInt(document.getElementById("monto_prestamo").value);
+    const periodo = parseInt(document.getElementById("cantidad_cuotas").value);
+
+    while (prestamo < 100000) {
+        if (prestamo < 100000) {
+            alert("El monto ingresado debe ser mayor a $100,000. Intente de nuevo");
+        }
     }
-}
 
-function calcularCuota(prestamo, tasaDeInteres, periodo) {
-    // Convierte la tasa de interés anual a tasa periódica
-    const tasaPeriodica = tasaDeInteres / 12; // Suponiendo que la tasa es anual y se paga mensualmente
+    function calcularCuota(prestamo, tasaDeInteres, periodo) {
+        // Convierte la tasa de interés anual a tasa periódica
+        const tasaPeriodica = tasaDeInteres / 12; // Suponiendo que la tasa es anual y se paga mensualmente
 
-    // Calcula la cuota utilizando la fórmula del sistema de amortización francés
-    const cuota = (prestamo * tasaPeriodica * Math.pow(1 + tasaPeriodica, periodo)) / (Math.pow(1 + tasaPeriodica, periodo) - 1);
+        // Calcula la cuota utilizando la fórmula del sistema de amortización francés
+        const cuota = (prestamo * tasaPeriodica * Math.pow(1 + tasaPeriodica, periodo)) / (Math.pow(1 + tasaPeriodica, periodo) - 1);
 
-    return cuota;
-}
-
-while (periodo !== 6 && periodo !== 12 && periodo !== 18 && periodo !== 24) {
-    periodo = parseInt(prompt("En cuántas cuotas quiere pagar el crédito? (6, 12, 18 o 24)"));
-
-    if (periodo !== 6 && periodo !== 12 && periodo !== 18 && periodo !== 24) {
-        alert("El número de cuotas ingresado no es válido");
-    } else {
-        cuota= calcularCuota (prestamo, tasaDeInteres, periodo)
+        return cuota;
     }
-}
 
-alert("usted va a pagar " + periodo + " cuotas de $" + cuota.toFixed(2))
+    cuota = calcularCuota(prestamo, tasaDeInteres, periodo); // Asignación de cuota
+
+    alert("Usted va a pagar " + periodo + " cuotas de $" + cuota.toFixed(2).toLocaleString('es-ES'));
+});
 
 
